b710500a66b339b4697de334580f52f14dd31675
SPARTAN-042 <sentinal042@gmail.com>
2022-08-13 11:04:45 +0100

    PasswordEncoder and WebSecurityConfig added

--- src/main/java/com/Courier/CourierManagementSystem/CourierManagementSystemApplication.java
+++ src/main/java/com/Courier/CourierManagementSystem/CourierManagementSystemApplication.java
@@ -2,8 +2,6 @@ package com.Courier.CourierManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
---
+++ src/main/java/com/Courier/CourierManagementSystem/security/PasswordEncoder.java
@@ -0,0 +1,4 @@
+package com.Courier.CourierManagementSystem.security;
+
+public class PasswordEncoder {
+}
