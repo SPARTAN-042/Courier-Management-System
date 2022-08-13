5d9776d30fc75273bfb174f6ee6cb401dc5016cd
SPARTAN-042 <sentinal042@gmail.com>
2022-08-13 11:03:55 +0100

    Registration package and files added

---
+++ src/main/java/com/Courier/CourierManagementSystem/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.Courier.CourierManagementSystem.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
